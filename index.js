6b1bb1a460c6d2d340171d27ec3a947d23575f51
Jorge Brandon Chandomi Esponda <jorge_b_chandomi@outlook.com>
2024-08-04 21:11:54 -0600

    simple deploy

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import { Command } from "commander";
 import {GearApi, GearKeyring} from "@gear-js/api";
 import fs from "fs";
-import * as events from "node:events";
 
 const args = process.argv.slice(2);
 const program = new Command();
@@ -10,54 +9,12 @@ const gear_api = await GearApi.create({
 });
 
 program
-    .name("config")
-    .description("Config an account for deployments")
-    .command("set <mnemonic> <name>")
-    .action(async (mnemonic, name) => {
-        if (!fs.existsSync("./config")) {
-            const data = {
-                network_url: "wss://testnet.vara.network",
-                accounts: [
-                    {
-                        name: name,
-                        mnemonic: mnemonic,
-                        default: true
-                    }
-                ]
-            }
-        } else {
-            const data = JSON.parse(fs.readFileSync("./config", "utf-8"));
-            if (data.accounts.find(acc => acc.name === name)) {
-                console.log("Account with name already exists");
-                return;
-            }
-            data.accounts.push({
-                name: name,
-                mnemonic: mnemonic,
-                default: false
-            });
-            fs.writeFileSync("./config", JSON.stringify(data));
-        }
-    });
-
-program
-    .name("config")
-    .description("Change account")
-    .command("default <account_name>")
-    .action(async (account_name) => {
-        const data = JSON.parse(fs.readFileSync("./config", "utf-8"));
-        data.accounts.forEach(acc => {
-            acc.default = acc.name === account_name;
-        });
-        fs.writeFileSync("./config", JSON.stringify(data));
-    });
-
-program
     .name("guardian_protocol_gear_cli")
     .description("A simple CLI for CI/CD on GearProtocol")
-    .command("deploy <wasm_opt_path> <payload> <meta>")
-    .action(async (wasm_opt_path, payload, meta) => {
+    .command("deploy <mnemonic> <wasm_opt_path> <payload> <meta>")
+    .action(async (mnemonic, wasm_opt_path, payload, meta) => {
         const code = fs.readFileSync(wasm_opt_path);
+        const metadata = fs.readFileSync(meta, "utf-8");
         const payloadData = JSON.parse(fs.readFileSync(payload, "utf-8"));
         const program = {
             code: code,
@@ -66,13 +23,9 @@ program
             initPayload: payloadData
         }
         console.log("Creating upload extrinsic");
-        const { programId, codeId, salt, extrinsic } = await gear_api.program.upload(program, meta);
+        const { programId, codeId, salt, extrinsic } = await gear_api.program.upload(program, metadata);
 
-        const config = JSON.parse(fs.readFileSync("./config", "utf-8"));
-        const account = config.accounts.find(acc => acc.default);
-        console.log("Creating signer with: " + account.name)
-
-        const { seed} = GearKeyring.generateSeed(account.mnemonic);
+        const { seed} = GearKeyring.generateSeed(mnemonic);
         const signer = await GearKeyring.fromSeed(seed);
 
         console.log("Signing upload extrinsic");
